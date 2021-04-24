2834827aab568c015123eaf333deff835c78a5df
Nicolas Gomes <nicolasgomes56@gmail.com>
2021-04-24 01:40:29 -0300

    🚀 Projeto Aula-04

--- App.tsx
+++ App.tsx
@@ -21,14 +21,22 @@ export default function App() {
   });
 
   useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
     const subscription = Notifications.addNotificationReceivedListener(
       async notification => {
         const data = notification.request.content.data.plant as PlantProps;
         console.log(data);
-        
-      })
+      }
+    )
 
-      return () => subscription.remove();
+    return () => subscription.remove();
   }, [])
 
   if (!fontsLoaded) {
